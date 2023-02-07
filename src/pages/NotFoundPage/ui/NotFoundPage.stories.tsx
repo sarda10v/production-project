1735cd08c27a32a59fd384ac8703a343332c0f8b
sardalov <sarda10v@bk.ru>
2023-02-07 11:31:31 +0300

    feat: добавил и настроил скриншотные тесты Loki(регрессионное UI тестирование)

--- src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Theme } from "app/providers/ThemeProvider";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
-import NotFoundPage from "./NotFoundPage";
+import { NotFoundPage } from "./NotFoundPage";
 
 export default {
   title: "pages/NotFoundPage",
@@ -14,7 +14,9 @@ export default {
   },
 } as ComponentMeta<typeof NotFoundPage>;
 
-const Template: ComponentStory<typeof NotFoundPage> = (args) => <NotFoundPage {...args} />;
+const Template: ComponentStory<typeof NotFoundPage> = (args) => (
+  <NotFoundPage {...args} />
+);
 
 export const Normal = Template.bind({});
 Normal.args = {};

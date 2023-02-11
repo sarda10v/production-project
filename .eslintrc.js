b64f662de57cc8a8e4b1c3f3a33eba09fbfb98d3
sardalov <sarda10v@bk.ru>
2023-02-11 15:54:42 +0300

    feat: добавил модальное окно

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
       'react',
       '@typescript-eslint',
       'i18next',
+      "react-hooks"
   ],
   rules: {
       'react/jsx-indent': [2, 4],
@@ -49,6 +50,10 @@ module.exports = {
           },
       ],
       'max-len': ['error', { ignoreComments: true, code: 100 }],
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "error" // Checks effect dependencies
   },
   globals: {
       __IS_DEV__: true,
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,70 @@
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Modal.module.scss";
+
+interface ModalProps {
+  className?: string;
+  children?: ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
+}
+
+export const Modal = (props: ModalProps) => {
+  const { className, children, isOpen, onClose } = props;
+  const [isClosing, setIsClosing] = useState(false);
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+  const closeHandler = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true);
+      timerRef.current = setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, 300);
+    }
+  }, [onClose]);
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeHandler();
+      }
+    },
+    [closeHandler]
+  );
+
+  const onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener("keydown", onKeyDown);
+    }
+    return () => {
+      clearTimeout(timerRef.current);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [isOpen, onKeyDown]);
+
+  const mods: Record<string, boolean> = {
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
+  };
+
+  return (
+    <div className={classNames(cls.Modal, mods, [className])}>
+      <div className={cls.overlay} onClick={closeHandler}>
+        <div className={cls.content} onClick={onContentClick}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};

fd7e8bac916fe5a6665189b1f911f33d92b0a8d5
sardalov <sarda10v@bk.ru>
2023-03-23 12:10:42 +0300

    feat: написал тест на сервис профиля validateProfileData

--- src/entities/Profile/model/services/validateProfileData/validateProfileData.test.ts
+++ src/entities/Profile/model/services/validateProfileData/validateProfileData.test.ts
@@ -1,56 +1,56 @@
-// import { Country } from 'entities/Country';
-// import { Currency } from 'entities/Currency';
-// import { ValidateProfileError } from 'entities/Profile';
-// import { validateProfileData } from './validateProfileData';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { ValidateProfileError } from 'entities/Profile';
+import { validateProfileData } from './validateProfileData';
 
-// const data = {
-//     username: 'admin',
-//     age: 22,
-//     country: Country.Ukraine,
-//     lastname: 'ulbi tv',
-//     first: 'asd',
-//     city: 'asf',
-//     currency: Currency.USD,
-// };
+const data = {
+    username: 'admin',
+    age: 22,
+    country: Country.Ukraine,
+    lastname: 'ulbi tv',
+    firstname: 'asd',
+    city: 'asf',
+    currency: Currency.USD,
+};
 
-// describe('validateProfileData.test', () => {
-//     test('success', async () => {
-//         const result = validateProfileData(data);
+describe('validateProfileData.test', () => {
+    test('success', async () => {
+        const result = validateProfileData(data);
 
-//         expect(result).toEqual([]);
-//     });
+        expect(result).toEqual([]);
+    });
 
-//     test('without first and last name', async () => {
-//         const result = validateProfileData({ ...data, first: '', lastname: '' });
+    test('without firstname and last name', async () => {
+        const result = validateProfileData({ ...data, firstname: '', lastname: '' });
 
-//         expect(result).toEqual([
-//             ValidateProfileError.INCORRECT_USER_DATA,
-//         ]);
-//     });
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+        ]);
+    });
 
-//     test('incorrect age', async () => {
-//         const result = validateProfileData({ ...data, age: undefined });
+    test('incorrect age', async () => {
+        const result = validateProfileData({ ...data, age: undefined });
 
-//         expect(result).toEqual([
-//             ValidateProfileError.INCORRECT_AGE,
-//         ]);
-//     });
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_AGE,
+        ]);
+    });
 
-//     test('incorrect country', async () => {
-//         const result = validateProfileData({ ...data, country: undefined });
+    test('incorrect country', async () => {
+        const result = validateProfileData({ ...data, country: undefined });
 
-//         expect(result).toEqual([
-//             ValidateProfileError.INCORRECT_COUNTRY,
-//         ]);
-//     });
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_COUNTRY,
+        ]);
+    });
 
-//     test('incorrect all', async () => {
-//         const result = validateProfileData({});
+    test('incorrect all', async () => {
+        const result = validateProfileData({});
 
-//         expect(result).toEqual([
-//             ValidateProfileError.INCORRECT_USER_DATA,
-//             ValidateProfileError.INCORRECT_AGE,
-//             ValidateProfileError.INCORRECT_COUNTRY,
-//         ]);
-//     });
-// });
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+            ValidateProfileError.INCORRECT_AGE,
+            ValidateProfileError.INCORRECT_COUNTRY,
+        ]);
+    });
+});

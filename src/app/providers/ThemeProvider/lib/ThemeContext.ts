5ec3ff946dde3e35515a43f00e0bb8bc072c80e0
sardalov <sarda10v@bk.ru>
2023-03-23 14:31:18 +0300

    feat: добавил шаблон новой темы

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 export enum Theme {
   LIGHT = 'app_light_theme',
   DARK = 'app_dark_theme',
+  // BLACK = 'app_black_theme',
 }
 export interface ThemeContextProps {
   theme?: Theme;
